38daa3eb174310ad07c491ddb2da52b22b90babd
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-05 10:17:41 +0430

    adding fund_app views

--- fund_app/views.py
+++ fund_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import InvestmentFund, PriceChange, UserInvestment, InvestmentHistory
+from .serializers import InvestmentFundSerializer, PriceChangeSerializer, UserInvestmentSerializer, InvestmentHistorySerializer
 
-# Create your views here.
+class InvestmentFundViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentFund.objects.all()
+    serializer_class = InvestmentFundSerializer
+
+class PriceChangeViewSet(viewsets.ModelViewSet):
+    queryset = PriceChange.objects.all()
+    serializer_class = PriceChangeSerializer
+
+class UserInvestmentViewSet(viewsets.ModelViewSet):
+    queryset = UserInvestment.objects.all()
+    serializer_class = UserInvestmentSerializer
+
+class InvestmentHistoryViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentHistory.objects.all()
+    serializer_class = InvestmentHistorySerializer
